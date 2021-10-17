81f009e37df52e20b113f3e6da783c5a783d3ca1
Giovanni Zarrillo <gazarrillo1105@gmail.com>
2021-10-17 19:30:45 -0400

    reset using a different template

--- project/src/models/train_model.R
+++ project/src/models/train_model.R
@@ -25,7 +25,47 @@ drops <- c('id')
 train <- train[, !drops, with = FALSE]
 test <- test[, !drops, with = FALSE]
 
-# fit a glmnet model -------------------------------------------------------
+
+# fit a linear model ------------------------------------------------------
+
+model0 <- lm(future_price~., data=train)
+summary(model0)
+pred <- predict(model0, newdata=test)
+
+sample_sub <- fread("./project/volume/data/raw/sample_sub.csv")
+sample_sub$future_price <- pred
+fwrite(sample_sub, "./project/volume/data/processed/submit_lm.csv")
+
+
+# make a submission file with merge ---------------------------------------
+
+### instead of simple replacing future_price with pred,
+### merging is a safer approach
+
+train <- fread("./project/volume/data/interim/train.csv")
+test <- fread("./project/volume/data/interim/test.csv")
+train <- train[!duplicated(train),]
+train_y <- train$future_price
+
+### we will keep id in train and test
+### but do lm and prediction without id
+drops <- c('id')
+model0 <- lm(future_price~., data=train[, !drops, with=F])
+pred <- predict(model0, newdata=test[, !drops, with=F])
+result <- data.table(id=test$id, future_price=pred)
+
+### load the sample submission file and remove future_price
+sample_sub <- fread("./project/volume/data/raw/sample_sub.csv")
+sample_sub$future_price <- NULL
+
+### merge sample_sub with result while keeping the order
+setkey(sample_sub, id)
+setkey(result, id)
+sample_sub <- merge(sample_sub, result, sort=F)
+fwrite(sample_sub, "./project/volume/data/processed/submit_lm.csv")
+
+
+# fit a lasso model -------------------------------------------------------
 
 drops <- c('id')
 train <- train[, !drops, with = FALSE]
@@ -53,7 +93,7 @@ test <- data.table(test)
 train <- as.matrix(train)
 
 ### fit a model
-model1 <- cv.glmnet(train, train_y, alpha=0, family="gaussian")
+model1 <- cv.glmnet(train, train_y, alpha=1, family="gaussian")
 
 ### choose the best lambda
 best_lambda <- model1$lambda.min
@@ -64,7 +104,7 @@ predict(model1, s=best_lambda, newx=test, type="coefficients")
 
 # Fit a full model --------------------------------------------------------
 
-model2 <- glmnet(train, train_y, alpha=0, family="gaussian")
+model2 <- glmnet(train, train_y, alpha=1, family="gaussian")
 
 plot_glmnet(model2)
 
@@ -86,4 +126,4 @@ sample_sub <- fread("./project/volume/data/raw/sample_sub.csv")
 
 sample_sub$future_price <- pred
 
-fwrite(sample_sub, "./project/volume/data/processed/submit_elastic-net.csv")
+fwrite(sample_sub, "./project/volume/data/processed/submit_lasso.csv")
